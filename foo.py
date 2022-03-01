5a440acfa312220b289f86c88787b735b8ed5fda
Will Schmitt <will@rightfoot.com>
2022-03-01 11:23:58 -0800

    [RF-1] Add support to greeter to allow specification of who to greet.
    No longer hardcodes "world" to be greeted; now supports a person to be greeted.

--- foo.py
+++ foo.py
@@ -1,6 +1,6 @@
-def hello_world():
-    print("Hello, world.")
+def hello_world(who: str):
+    print("Hello, {}.".format(who))
 
 if __name__ == "__main__":
-    hello_world()
+    hello_world("world")
 
